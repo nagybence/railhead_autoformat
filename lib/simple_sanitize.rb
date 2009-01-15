e393647a62fe229eef2b18f531f2dc0e8667176a
Bence Nagy <nagybence@tipogral.hu>
2009-01-15 14:45:26 +0100

    Automatically strip input.

--- lib/simple_sanitize.rb
+++ lib/simple_sanitize.rb
@@ -5,28 +5,28 @@ module SimpleSanitize
     base.extend ActionView::Helpers::SanitizeHelper::ClassMethods
     base.class_eval do
       before_save :sanitize_fields
-      class_inheritable_reader :simple_sanitize
+      class_inheritable_reader :simple_sanitize_options
     end
   end
 
   module ClassMethods
     def sanitize_fields(options = {})
-      write_inheritable_attribute(:simple_sanitize, {
+      write_inheritable_attribute(:simple_sanitize_options, {
         :except => (options[:except] || []),
         :allow_tags => (options[:allow_tags] || [])
       })
     end
     alias_method :sanitize_field, :sanitize_fields
   end
-    
+
   def sanitize_fields
     self.class.columns.each do |column|
       next unless (column.type == :string || column.type == :text)
       field = column.name.to_sym
-      value = self[field]
-      if simple_sanitize && simple_sanitize[:except].include?(field)
-        next
-      elsif simple_sanitize && simple_sanitize[:allow_tags].include?(field)
+      value = self[field].strip
+      if simple_sanitize_options && simple_sanitize_options[:except].include?(field)
+        self[field] = value
+      elsif simple_sanitize_options && simple_sanitize_options[:allow_tags].include?(field)
         self[field] = sanitize(value)
       else
         self[field] = strip_tags(value)
