73dd7518635d52d260b2df3c17af25e75e5263ac
Bence Nagy <nagybence@tipogral.hu>
2009-01-27 11:33:46 +0100

    SimpleSanitize -> RailheadSanitize

--- lib/railhead_sanitize.rb
+++ lib/railhead_sanitize.rb
@@ -1,22 +1,21 @@
-module SimpleSanitize
+module RailheadSanitize
   def self.included(base)
     base.extend ClassMethods
     base.send :include, ActionView::Helpers::SanitizeHelper
     base.extend ActionView::Helpers::SanitizeHelper::ClassMethods
     base.class_eval do
+      class_inheritable_reader :sanitize_options
       before_validation :sanitize_fields
-      class_inheritable_reader :simple_sanitize_options
     end
   end
 
   module ClassMethods
-    def sanitize_fields(options = {})
-      write_inheritable_attribute(:simple_sanitize_options, {
+    def auto_sanitize(options = {})
+      write_inheritable_attribute(:sanitize_options, {
         :except => (options[:except] || []),
         :allow_tags => (options[:allow_tags] || [])
       })
     end
-    alias_method :sanitize_field, :sanitize_fields
   end
 
   def sanitize_fields
@@ -24,9 +23,9 @@ module SimpleSanitize
       next unless (column.type == :string || column.type == :text)
       field = column.name.to_sym
       value = self[field] && self[field].strip
-      if simple_sanitize_options && simple_sanitize_options[:except].include?(field)
+      if sanitize_options && sanitize_options[:except].include?(field)
         self[field] = value
-      elsif simple_sanitize_options && simple_sanitize_options[:allow_tags].include?(field)
+      elsif sanitize_options && sanitize_options[:allow_tags].include?(field)
         self[field] = sanitize(value)
       else
         self[field] = strip_tags(value)
@@ -35,4 +34,4 @@ module SimpleSanitize
   end
 end
 
-ActiveRecord::Base.send :include, SimpleSanitize
+ActiveRecord::Base.send :include, RailheadSanitize
